3c6e807877f2aaa3b8948636b62c1e96ba2dcfb1
Tobias Bocanegra <tripod@apache.org>
2017-10-17 07:32:07 +0000

    JCRVLT-211 only validate the order within the package on the filter root level
    (this closes #1)
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/commons/filevault-package-maven-plugin/trunk@1812360 13f79535-47bb-0310-9956-ffa450edef68

--- it/src/test/java/org/apache/jackrabbit/filevault/maven/packaging/DefaultProjectTest.java
+++ it/src/test/java/org/apache/jackrabbit/filevault/maven/packaging/DefaultProjectTest.java
@@ -17,6 +17,8 @@
 package org.apache.jackrabbit.filevault.maven.packaging;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -24,7 +26,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 import org.apache.maven.it.util.FileUtils;
-import org.apache.maven.it.util.StringUtils;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -44,13 +46,15 @@ public class DefaultProjectTest extends PackageTestBase {
         File testPackageFile = buildProject(getDefaultProperties());
         assertThat(testPackageFile.exists(), is(true));
 
-        String expected = FileUtils.fileRead(new File(testProjectDir, "expected-files.txt"));
+        List<String> expectedEntries = Files.readAllLines(new File(testProjectDir, "expected-files.txt").toPath(), StandardCharsets.UTF_8);
+        List<String> expectedEntriesInOrder= Files.readAllLines(new File(testProjectDir, "expected-file-order.txt").toPath(), StandardCharsets.UTF_8);
 
         List<String> entries = new ArrayList<String>();
-        JarFile jar = new JarFile(testPackageFile);
-        Enumeration<JarEntry> e = jar.entries();
-        while (e.hasMoreElements()) {
-            entries.add(e.nextElement().getName());
+        try (JarFile jar = new JarFile(testPackageFile)) {
+            Enumeration<JarEntry> e = jar.entries();
+            while (e.hasMoreElements()) {
+                entries.add(e.nextElement().getName());
+            }
         }
 
         // ensure that MANIFEST.MF is first entry
@@ -60,9 +64,11 @@ public class DefaultProjectTest extends PackageTestBase {
         }
         assertEquals("MANIFEST.MF must be first entry", "META-INF/MANIFEST.MF", first);
 
-        // (we don't sort the files so that we can test if the filter order is respected)
-        String result = StringUtils.join(entries.iterator(),"\n");
-        assertEquals("File List", expected, result);
+        // first check that only the expected entries are there in the package (regardless of the order)
+        assertThat("Package contains the expected entry names", entries, Matchers.containsInAnyOrder(expectedEntries.toArray()));
+        
+        // then check order of some of the entries
+        assertThat("Order of entries within package", entries, Matchers.containsInRelativeOrder(expectedEntriesInOrder.toArray()));
 
         String expectedManifest = FileUtils.fileRead(new File(testProjectDir, "expected-manifest.txt"));
         verifyManifest(testPackageFile, expectedManifest);
