19333aab6f361352c188da4ed01b3cb3efb9357f
Konrad Windszus <kwin@apache.org>
2021-06-22 19:45:26 +0200

    JCRVLT-537 improve subpackage detection

--- src/test/java/org/apache/jackrabbit/filevault/maven/packaging/it/ValidatePackageIT.java
+++ src/test/java/org/apache/jackrabbit/filevault/maven/packaging/it/ValidatePackageIT.java
@@ -33,20 +33,26 @@ public class ValidatePackageIT {
 
     private static final String TEST_PROJECT_NAME = "/validator-projects/";
 
-    private ProjectBuilder verify(String projectName) throws VerificationException, IOException {
+    private ProjectBuilder verify(String projectName, boolean expectToFail) throws VerificationException, IOException {
         return new ProjectBuilder()
                 .setTestProjectDir(TEST_PROJECT_NAME + projectName)
-                .setBuildExpectedToFail(true)
-                .build()
-                .verifyExpectedLogLines(Paths.get("META-INF", "vault", "filter.xml").toString());
+                .setBuildExpectedToFail(expectToFail)
+                .build();
     }
 
     @Test
     public void testInvalidProject() throws Exception {
-        File csvReportFile = new File(verify("invalid-project").getTestProjectDir(), "report.csv");
+        ProjectBuilder projectBuilder = verify("invalid-project", true);
+        projectBuilder.verifyExpectedLogLines(Paths.get("META-INF", "vault", "filter.xml").toString());
+        File csvReportFile = new File(projectBuilder.getTestProjectDir(), "report.csv");
         Assert.assertTrue(csvReportFile.exists());
         CSVParser csvParser = CSVParser.parse(csvReportFile, StandardCharsets.UTF_8, CSVFormat.EXCEL);
         List<CSVRecord> actualRecords = csvParser.getRecords();
         Assert.assertEquals(4, actualRecords.size()); // 3 issues + header
+    }
+ 
+    @Test
+    public void testValidProjectWithZip() throws Exception {
+        verify("project-with-zip", false);
     }
 }
